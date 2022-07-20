38a060c554cf478ca8bad5196d9370c89b9dbd5a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-07-20 12:12:25 -0700

    initial commits

--- consts.go
+++ consts.go
@@ -1,9 +1,9 @@
 package dtxt
 
 const (
-	esc = 27
-	fs  = 28
-	gs  = 29
-	rs  = 30
-	us  = 31
+	esc = 27 // Escape control code character
+	fs  = 28 // File Separator control code character
+	gs  = 29 // Group Separator (i.e., Table Separator) control code character
+	rs  = 30 // Row Separator control code character
+	us  = 31 // Unit Separator (i.e., Field Separator) control code character
 )
